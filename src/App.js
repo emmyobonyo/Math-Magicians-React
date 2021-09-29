bde7bdd7a5b822515e7cd651ff83ff5a6b2ba673
emmyobonyo@gmail.com <emmyobonyo@gmail.com>
2021-09-29 20:35:32 +0300

    Change App.js to use class components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
+import React from 'react';
 import Calculator from './components/calculator';
 import './App.css';
 
-function App() {
-  return (
-    <div>
-      <Calculator />
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>
+        <Calculator />
+      </div>
+    );
+  }
 }
 
 export default App;
