e3a902fd20eb6373ecbeb7e6330a8dd7de45e4d9
emmyobonyo@gmail.com <emmyobonyo@gmail.com>
2021-09-30 14:50:46 +0300

    Add name attribute to button element

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -13,27 +13,27 @@ export default class Calculator extends React.Component {
           <p className="calculator-digits">0</p>
         </div>
         <div className="calculator-buttons">
-          <div className="calculator-button">AC</div>
-          <div className="calculator-button">+/-</div>
-          <div className="calculator-button">%</div>
-          <div className="calculator-button orange-button">&#247;</div>
-          <div className="calculator-button">7</div>
-          <div className="calculator-button">8</div>
-          <div className="calculator-button">9</div>
-          <div className="calculator-button orange-button">&times;</div>
-          <div className="calculator-button">4</div>
-          <div className="calculator-button">5</div>
-          <div className="calculator-button">6</div>
-          <div className="calculator-button orange-button">&#8722;</div>
-          <div className="calculator-button">1</div>
-          <div className="calculator-button">2</div>
-          <div className="calculator-button">3</div>
-          <div className="calculator-button orange-button">+</div>
+          <button name="AC" className="calculator-button" type="button">AC</button>
+          <button name="plus-or-minus" className="calculator-button" type="button">+/-</button>
+          <button name="mod" className="calculator-button" type="button">%</button>
+          <button name="/" className="calculator-button orange-button" type="button">&#247;</button>
+          <button name="7" className="calculator-button" type="button">7</button>
+          <button name="8" className="calculator-button" type="button">8</button>
+          <button name="9" className="calculator-button" type="button">9</button>
+          <button name="*" className="calculator-button orange-button" type="button">&times;</button>
+          <button name="4" className="calculator-button" type="button">4</button>
+          <button name="5" className="calculator-button" type="button">5</button>
+          <button name="6" className="calculator-button" type="button">6</button>
+          <button name="-" className="calculator-button orange-button" type="button">&#8722;</button>
+          <button name="1" className="calculator-button" type="button">1</button>
+          <button name="2" className="calculator-button" type="button">2</button>
+          <button name="3" className="calculator-button" type="button">3</button>
+          <button name="+" className="calculator-button orange-button" type="button">+</button>
         </div>
         <div className="calculator-buttons-bottom">
-          <div className="calculator-button-bottom button-one">0</div>
-          <div className="calculator-button-bottom button-two">.</div>
-          <div className="calculator-button-bottom button-three orange-button-bottom">=</div>
+          <button name="0" className="calculator-button-bottom button-one" type="button">0</button>
+          <button name="." className="calculator-button-bottom button-two" type="button">.</button>
+          <button name="=" className="calculator-button-bottom button-three orange-button-bottom" type="button">=</button>
         </div>
       </div>
     );
