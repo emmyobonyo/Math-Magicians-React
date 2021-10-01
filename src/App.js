145aa99ada1c81d999e3bb24149e9ef91d1de17e
emmyobonyo@gmail.com <emmyobonyo@gmail.com>
2021-10-01 13:42:15 +0300

    implement calculator functionality

--- src/App.js
+++ src/App.js
@@ -6,14 +6,25 @@ import calculate from './logic/calculate';
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { total: 0 };
+    this.state = { total: 0, next: null, operation: null };
+    this.handleEvent = this.handleEvent.bind(this);
+  }
+
+  handleEvent = (e) => {
+    const obj = calculate(this.state, e.target.innerHTML);
+    this.setState(obj);
   }
 
   render() {
-    const { total } = this.state;
+    const { total, next, operation } = this.state;
     return (
       <div>
-        <Calculator total={total} calculate={calculate} />
+        <Calculator
+          total={total !== null ? total : ''}
+          next={next !== null ? next : ''}
+          operation={operation !== null ? operation : ''}
+          onClickHandler={this.handleEvent}
+        />
       </div>
     );
   }
--- src/components/calculator.js
+++ src/components/calculator.js
@@ -7,34 +7,39 @@ export default class Calculator extends React.Component {
   }
 
   render() {
-    const { total, calculate } = this.props;
+    const {
+      total,
+      next,
+      operation,
+      onClickHandler,
+    } = this.props;
     return (
       <div className="calculator">
         <div className="calculator-screen">
-          <p className="calculator-digits">{total}</p>
+          <p className="calculator-digits">{`${total} ${next} ${operation}`}</p>
         </div>
         <div className="calculator-buttons">
-          <button className="calculator-button" type="button" onClick={calculate}>AC</button>
-          <button className="calculator-button" type="button" onClick={calculate}>+/-</button>
-          <button className="calculator-button" type="button" onClick={calculate}>%</button>
-          <button className="calculator-button orange-button" type="button" onClick={calculate}>&#247;</button>
-          <button className="calculator-button" type="button" onClick={calculate}>7</button>
-          <button className="calculator-button" type="button" onClick={calculate}>8</button>
-          <button className="calculator-button" type="button" onClick={calculate}>9</button>
-          <button className="calculator-button orange-button" type="button" onClick={calculate}>&times;</button>
-          <button className="calculator-button" type="button" onClick={calculate}>4</button>
-          <button className="calculator-button" type="button" onClick={calculate}>5</button>
-          <button className="calculator-button" type="button" onClick={calculate}>6</button>
-          <button className="calculator-button orange-button" type="button" onClick={calculate}>&#8722;</button>
-          <button className="calculator-button" type="button" onClick={calculate}>1</button>
-          <button className="calculator-button" type="button" onClick={calculate}>2</button>
-          <button className="calculator-button" type="button" onClick={calculate}>3</button>
-          <button className="calculator-button orange-button" type="button" onClick={calculate}>+</button>
+          <button className="calculator-button" type="button" onClick={onClickHandler}>AC</button>
+          <button className="calculator-button" type="button" onClick={onClickHandler}>+/-</button>
+          <button className="calculator-button" type="button" onClick={onClickHandler}>%</button>
+          <button className="calculator-button orange-button" type="button" onClick={onClickHandler}>/</button>
+          <button className="calculator-button" type="button" onClick={onClickHandler}>7</button>
+          <button className="calculator-button" type="button" onClick={onClickHandler}>8</button>
+          <button className="calculator-button" type="button" onClick={onClickHandler}>9</button>
+          <button className="calculator-button orange-button" type="button" onClick={onClickHandler}>x</button>
+          <button className="calculator-button" type="button" onClick={onClickHandler}>4</button>
+          <button className="calculator-button" type="button" onClick={onClickHandler}>5</button>
+          <button className="calculator-button" type="button" onClick={onClickHandler}>6</button>
+          <button className="calculator-button orange-button" type="button" onClick={onClickHandler}>-</button>
+          <button className="calculator-button" type="button" onClick={onClickHandler}>1</button>
+          <button className="calculator-button" type="button" onClick={onClickHandler}>2</button>
+          <button className="calculator-button" type="button" onClick={onClickHandler}>3</button>
+          <button className="calculator-button orange-button" type="button" onClick={onClickHandler}>+</button>
         </div>
         <div className="calculator-buttons-bottom">
-          <button className="calculator-button-bottom button-one" type="button" onClick={calculate}>0</button>
-          <button className="calculator-button-bottom button-two" type="button" onClick={calculate}>.</button>
-          <button className="calculator-button-bottom button-three orange-button-bottom" type="button" onClick={calculate}>=</button>
+          <button className="calculator-button-bottom button-one" type="button" onClick={onClickHandler}>0</button>
+          <button className="calculator-button-bottom button-two" type="button" onClick={onClickHandler}>.</button>
+          <button className="calculator-button-bottom button-three orange-button-bottom" type="button" onClick={onClickHandler}>=</button>
         </div>
       </div>
     );
