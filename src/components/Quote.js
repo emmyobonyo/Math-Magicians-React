de584fad95345b9a10e5ef10a8eb1776656f073e
emmyobonyo@gmail.com <emmyobonyo@gmail.com>
2021-10-06 18:35:02 +0300

    Fix CSS linter errors

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -2,8 +2,14 @@ import React from 'react';
 
 function Quote() {
   return (
-    <div>
-      <h3>Quote</h3>
+    <div className="quote-page">
+      <h2>
+        Programming is not about numbers,
+        data structures, variables, classes,
+        debugging, git-flow, cramming,
+        copying and pasting, stack-overflow
+        or even functions. It is about understanding.
+      </h2>
     </div>
   );
 }
