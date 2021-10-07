53219d607e05b0929d340044299ae93994342150
Busiwa24 <busiwaliuma@gmail.com>
2021-10-07 14:56:02 +0200

    add multiplication and division tests

--- src/logic/__test__/operate.test.js
+++ src/logic/__test__/operate.test.js
@@ -3,3 +3,9 @@ import operate from './operate';
 test('adds 1+1 to equal 2', () => {
   expect(operate(1, 1, '+')).toBe('2');
 });
+test('subtract 2-1 to equal 1', () => {
+    expect(operate(2, 1, '-')).toBe('1');
+  });
+  test('divide 12÷2 to equal 6', () => {
+    expect(operate(12, 2, '÷')).toBe('6');
+  });
