241bacf6e96ad44786e0a1517f0b40542734a498
emmyobonyo@gmail.com <emmyobonyo@gmail.com>
2021-10-07 13:57:06 +0300

    Add test to see that a decimal is added if the next number is not a decimal

--- src/logic/__test__/calculate.test.js
+++ src/logic/__test__/calculate.test.js
@@ -1,1 +1,36 @@
 import calculate from '../calculate';
+
+test('AC clears the screen', () => {
+  const obj = {
+    total: 15,
+    next: 90,
+    operation: '+'
+  };
+  expect(calculate(obj, 'AC')).toEqual({
+    total: 0,
+    next: null,
+    operation: null
+  });
+});
+
+test('to see that double decimals are not added', () => {
+  const obj = {
+    total: null,
+    next: '9.',
+    operation: null
+  }
+  expect(calculate(obj, '.')).toEqual({
+    ...obj
+  })
+})
+
+test('to see that the next variable is given a value', () => {
+  const obj = {
+    total: null,
+    next: '9',
+    operation: null
+  }
+  expect(calculate(obj, '.')).toEqual({
+    ...obj, next: `${obj.next}.`,
+  })
+})
