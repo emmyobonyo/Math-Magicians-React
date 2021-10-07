7c55e41da27b2f0166118977008361937edb30f1
emmyobonyo@gmail.com <emmyobonyo@gmail.com>
2021-10-07 12:18:44 +0300

    Add tests

--- src/components/__test__/App.test.js
+++ src/components/__test__/App.test.js
@@ -1,9 +1,67 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import renderer from 'react-test-renderer';
+import { screen, render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from '../../App';
+import CalculatorPage from '../CalculatorPage';
+import Navigation from '../Navigation';
+import Home from '../Home';
+import Quote from '../Quote';
+import Calculator from '../calculator';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+it('matches snapshot', () => {
+  const tree = renderer.create(<App />).toJSON();
+  expect(tree).toMatchSnapshot();
+})
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<CalculatorPage />, div);
+  ReactDOM.unmountComponentAtNode(div);
+})
+
+it('matches snapshot', () => {
+  const tree = renderer.create(<CalculatorPage />).toJSON();
+  expect(tree).toMatchSnapshot();
+})
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<Navigation />, div);
+  ReactDOM.unmountComponentAtNode(div);
+})
+
+it('matches snapshot', () => {
+  const tree = renderer.create(<Navigation />).toJSON();
+  expect(tree).toMatchSnapshot();
+})
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<Home />, div);
+  ReactDOM.unmountComponentAtNode(div);
+})
+
+it('matches snapshot', () => {
+  const tree = renderer.create(<Home />).toJSON();
+  expect(tree).toMatchSnapshot();
+})
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<Quote />, div);
+  ReactDOM.unmountComponentAtNode(div);
+})
+
+it('should call a click event', () => {
+  render(<Calculator />);
+  expect(userEvent.click(screen.getByTestId('ac'))).toHaveBeenCalled;
+})
+
