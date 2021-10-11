a8ff81adf254c86cb8e6dcd999df0645b0955a04
emmyobonyo@gmail.com <emmyobonyo@gmail.com>
2021-10-11 13:55:10 +0300

    Add paragraph

--- src/components/Home.js
+++ src/components/Home.js
@@ -21,7 +21,7 @@ function Home() {
         Arithmetic operations? You may have to buy an actual
         calculator for that.
       </p>
-
+      <p>This is to check that the server is working</p>
     </div>
   );
 }
